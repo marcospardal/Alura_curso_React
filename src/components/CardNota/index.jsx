09a499df84b6c6b07b9725bb46ca98d2c978e8a9
Marcos Davi Nascimento <marcosnascimento@eng.ci.ufpb.br>
2021-12-24 11:42:10 -0300

    ajustando posição da categoria

--- src/components/CardNota/index.jsx
+++ src/components/CardNota/index.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import deleteIcon from '../../assets/img/delete.svg';
+import React, { Component } from "react";
+import deleteIcon from "../../assets/img/delete.svg";
 
-import './estilo.css';
+import "./estilo.css";
 
 class CardNota extends Component {
   render() {
     return (
-      <section className='card-nota'>
-        <header className='card-nota_cabecalho'>
-          <h3 className='card-nota_titulo'>{this.props.titulo}</h3>
-          <img 
-            src={deleteIcon} 
-            style={{ height: 15, cursor: 'pointer' }}
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <img
+            src={deleteIcon}
+            style={{ height: 15, cursor: "pointer" }}
             onClick={() => this.props.apagarNota(this.props.index)}
           />
-          <h4>{this.props.categoria}</h4>
         </header>
-        <p className='card-nota_texto'>{this.props.texto}</p>
+        <h4>{this.props.categoria}</h4>
+        <p className="card-nota_texto">{this.props.texto}</p>
       </section>
-    )
+    );
   }
 }
 
