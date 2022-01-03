c6f0d336083492143ba5cc2772f288d03100c6a0
Marcos Davi Nascimento <marcosnascimento@eng.ci.ufpb.br>
2022-01-03 10:00:53 -0300

    adicionando rota de página não encontrada

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -4,13 +4,15 @@ import "./assets/css/base/base.css";
 
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
+import Pagina404 from "./paginas/404";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Pagina404 />} />
       </Routes>
     </Router>
   );
